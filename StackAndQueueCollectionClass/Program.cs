f0b5eaa1b6b4ab20d00fcbea57993753aeaba2ae
Subhash Barela <barelasubhash16@gmail.com>
2023-07-08 10:28:34 +0530

    [subhash] Create Stack Data structure

---
+++ StackAndQueueCollectionClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueCollectionClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.push(56);
+            stack.push(30);
+            stack.push(70);
+            Console.WriteLine("1: Create Stack");
+            Console.WriteLine("Enter the choice");
+            int choice=int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("............................");
+                        stack.Display();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Please insert valid input");
+                        break;
+                    }
+            }
+            
+            Console.Read();
+        }
+    }
+}
